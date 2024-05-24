1850c677826bf6f2ab9e39b7451ade528e27d697
muk-jjang <whgdk0911@khu.ac.kr>
2024-05-24 11:14:58 +0900

    add: u-net module

---
+++ src/data/unet_datamodule.py
@@ -0,0 +1,225 @@
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+from lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
+from torchvision.transforms import v2
+import os
+import pandas as pd
+from PIL import Image
+
+class PETDataset(Dataset):
+    def __init__(self, data_dir, dataset_type='train', transform=None):
+        if dataset_type == 'train':
+            self.annotation_file = os.path.join(data_dir, 'pet_clean_label_csv_file.csv')
+            self.img_dir = os.path.join(data_dir, '페트병_무색유색_오염없음_이미지')
+        # elif dataset_type == 'test':
+        #     self.annotation_file = os.path.join(data_dir, 'pet_balance_csv_file_TEST.csv')
+        #     self.img_dir = os.path.join(data_dir, 'test')
+
+        self.img_labels = pd.read_csv(self.annotation_file, header=0, names=['file_name', 'label'])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0] + '.jpg')
+        image = Image.open(img_path).convert("L")
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, image
+
+class PETDataModule(LightningDataModule):
+    """`LightningDataModule` for the MNIST dataset.
+
+    The MNIST database of handwritten digits has a training set of 60,000 examples, and a test set of 10,000 examples.
+    It is a subset of a larger set available from NIST. The digits have been size-normalized and centered in a
+    fixed-size image. The original black and white images from NIST were size normalized to fit in a 20x20 pixel box
+    while preserving their aspect ratio. The resulting images contain grey levels as a result of the anti-aliasing
+    technique used by the normalization algorithm. the images were centered in a 28x28 image by computing the center of
+    mass of the pixels, and translating the image so as to position this point at the center of the 28x28 field.
+
+    A `LightningDataModule` implements 7 key methods:
+
+    ```python
+        def prepare_data(self):
+        # Things to do on 1 GPU/TPU (not on every GPU/TPU in DDP).
+        # Download data, pre-process, split, save to disk, etc...
+
+        def setup(self, stage):
+        # Things to do on every process in DDP.
+        # Load data, set variables, etc...
+
+        def train_dataloader(self):
+        # return train dataloader
+
+        def val_dataloader(self):
+        # return validation dataloader
+
+        def test_dataloader(self):
+        # return test dataloader
+
+        def predict_dataloader(self):
+        # return predict dataloader
+
+        def teardown(self, stage):
+        # Called on every process in DDP.
+        # Clean up after fit or test.
+    ```
+
+    This allows you to share a full dataset without explaining how to download,
+    split, transform and process the data.
+
+    Read the docs:
+        https://lightning.ai/docs/pytorch/latest/data/datamodule.html
+    """
+
+    def __init__(
+        self,
+        data_dir: str = "/data2/local_datasets/recycling/clean/",
+        train_val_test_split: Tuple[float, float, float] = [0.8, 0.1, 0.1],
+        batch_size: int = 16,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ) -> None:
+        """Initialize a `MNISTDataModule`.
+
+        :param data_dir: The data directory. Defaults to `"data/"`.
+        :param train_val_test_split: The train, validation and test split. Defaults to `(55_000, 5_000, 10_000)`.
+        :param batch_size: The batch size. Defaults to `64`.
+        :param num_workers: The number of workers. Defaults to `0`.
+        :param pin_memory: Whether to pin memory. Defaults to `False`.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        # data transformations
+        self.transforms = v2.Compose(
+            [v2.ToTensor()]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+        self.batch_size_per_device = batch_size
+
+    @property
+    def num_classes(self) -> int:
+        """Get the number of classes.
+
+        :return: The number of MNIST classes (10).
+        """
+        return 4
+
+    # def prepare_data(self) -> None:
+    #     """Download data if needed. Lightning ensures that `self.prepare_data()` is called only
+    #     within a single process on CPU, so you can safely add your downloading logic within. In
+    #     case of multi-node training, the execution of this hook depends upon
+    #     `self.prepare_data_per_node()`.
+
+    #     Do not use it to assign state (self.x = y).
+    #     """
+    #     PETDataset(self.hparams.data_dir,transform=self.transforms)
+    #     PETDataset(self.hparams.data_dir, transform=self.transforms)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by Lightning before `trainer.fit()`, `trainer.validate()`, `trainer.test()`, and
+        `trainer.predict()`, so be careful not to execute things like random split twice! Also, it is called after
+        `self.prepare_data()` and there is a barrier in between which ensures that all the processes proceed to
+        `self.setup()` once the data is prepared and available for use.
+
+        :param stage: The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`. Defaults to ``None``.
+        """
+        # Divide batch size by the number of devices.
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+
+            original_dataset = PETDataset(self.hparams.data_dir, transform=self.transforms)
+            # augmented_dataset = PETDataset(self.hparams.data_dir, transform=data_augmentation_transforms)
+            # Concatenate original dataset with augmented dataset
+            #trainset = ConcatDataset([original_dataset, augmented_dataset])
+            self.data_train, self.data_val, self.data_test = random_split(
+                dataset=original_dataset,
+                lengths=self.hparams.train_val_test_split,
+                generator=torch.Generator().manual_seed(42),
+            )
+            # self.data_test = PETDataset(self.hparams.data_dir, transform=self.transforms)
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader.
+
+        :return: The train dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader.
+
+        :return: The validation dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader.
+
+        :return: The test dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def teardown(self, stage: Optional[str] = None) -> None:
+        """Lightning hook for cleaning up after `trainer.fit()`, `trainer.validate()`,
+        `trainer.test()`, and `trainer.predict()`.
+
+        :param stage: The stage being torn down. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
+            Defaults to ``None``.
+        """
+        pass
+
+    def state_dict(self) -> Dict[Any, Any]:
+        """Called when saving a checkpoint. Implement to generate and save the datamodule state.
+
+        :return: A dictionary containing the datamodule state that you want to save.
+        """
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        """Called when loading a checkpoint. Implement to reload datamodule state given datamodule
+        `state_dict()`.
+
+        :param state_dict: The datamodule state returned by `self.state_dict()`.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    _ = PETDataModule()
---
+++ src/models/unet_module.py
@@ -0,0 +1,267 @@
+from typing import Any, Dict, Tuple
+
+import torch
+from lightning import LightningModule
+from torchmetrics import MaxMetric, MeanMetric
+from torchmetrics.classification.accuracy import Accuracy
+from torchmetrics import MaxMetric, MeanMetric
+from torchmetrics.functional import peak_signal_noise_ratio, structural_similarity_index_measure
+import numpy as np
+import wandb
+
+wandb.init(project='UNet')
+wandb.run.name = 'UNet_no_aug'
+wandb.run.save()
+
+class UnetModule(LightningModule):
+    """Example of a `LightningModule` for MNIST classification.
+
+    A `LightningModule` implements 8 key methods:
+
+    ```python
+    def __init__(self):
+    # Define initialization code here.
+
+    def setup(self, stage):
+    # Things to setup before each stage, 'fit', 'validate', 'test', 'predict'.
+    # This hook is called on every process when using DDP.
+
+    def training_step(self, batch, batch_idx):
+    # The complete training step.
+
+    def validation_step(self, batch, batch_idx):
+    # The complete validation step.
+
+    def test_step(self, batch, batch_idx):
+    # The complete test step.
+
+    def predict_step(self, batch, batch_idx):
+    # The complete predict step.
+
+    def configure_optimizers(self):
+    # Define and configure optimizers and LR schedulers.
+    ```
+
+    Docs:
+        https://lightning.ai/docs/pytorch/latest/common/lightning_module.html
+    """
+
+    def __init__(
+        self,
+        net: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
+        scheduler: torch.optim.lr_scheduler,
+        compile: bool,
+    ) -> None:
+        """Initialize a `MNISTLitModule`.
+
+        :param net: The model to train.
+        :param optimizer: The optimizer to use for training.
+        :param scheduler: The learning rate scheduler to use for training.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.net = net
+
+        # loss function
+        self.criterion = torch.nn.MSELoss()
+
+        # metric objects for calculating and averaging SSIM and PSNR across batches
+        self.train_ssim = MeanMetric()
+        self.val_ssim = MeanMetric()
+        self.test_ssim = MeanMetric()
+
+        self.train_psnr = MeanMetric()
+        self.val_psnr = MeanMetric()
+        self.test_psnr = MeanMetric()
+
+        # for averaging loss across batches
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
+        self.test_loss = MeanMetric()
+
+        # for tracking best so far validation SSIM
+        self.val_ssim_best = MaxMetric()
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Perform a forward pass through the model `self.net`.
+
+        :param x: A tensor of images.
+        :return: A tensor of logits.
+        """
+        return self.net(x)
+
+    def on_train_start(self) -> None:
+        """Lightning hook that is called when training begins."""
+        # by default lightning executes validation step sanity checks before training starts,
+        # so it's worth to make sure validation metrics don't store results from these checks
+        self.val_loss.reset()
+        self.val_ssim.reset()
+        self.val_ssim_best.reset()
+        self.val_psnr.reset()
+
+
+    def model_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor]
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Perform a single model step on a batch of data.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target labels.
+
+        :return: A tuple containing (in order):
+            - A tensor of losses.
+            - A tensor of predictions.
+            - A tensor of target labels.
+        """
+        x, y = batch
+        restored = self.forward(x)
+        loss = self.criterion(restored, y)
+        return loss, restored, y
+
+    def training_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
+        """Perform a single training step on a batch of data from the training set.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target
+            labels.
+        :param batch_idx: The index of the current batch.
+        :return: A tensor of losses between model predictions and targets.
+        """
+        loss, restored, targets = self.model_step(batch)
+
+        # Calculate SSIM and PSNR
+        ssim_value = structural_similarity_index_measure(restored, targets)
+        psnr_value = peak_signal_noise_ratio(restored, targets)
+
+        # update and log metrics
+        self.train_loss(loss)
+        self.train_ssim(ssim_value)
+        self.train_psnr(psnr_value)
+
+        self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train/ssim", self.train_ssim, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train/psnr", self.train_psnr, on_step=False, on_epoch=True, prog_bar=True)
+        wandb.log({
+            "train/loss": self.train_loss.compute(), 
+            "train/ssim": self.train_ssim.compute(), 
+            "train/psnr": self.train_psnr.compute()
+        })
+
+        # return loss or backpropagation will fail
+        return loss
+
+    def on_train_epoch_end(self) -> None:
+        "Lightning hook that is called when a training epoch ends."
+        pass
+
+    def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
+        """Perform a single validation step on a batch of data from the validation set.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target
+            labels.
+        :param batch_idx: The index of the current batch.
+        """
+        loss, restored, targets = self.model_step(batch)
+
+        # Calculate SSIM and PSNR
+        ssim_value = structural_similarity_index_measure(restored, targets)
+        psnr_value = peak_signal_noise_ratio(restored, targets)
+
+        # update and log metrics
+        self.val_loss(loss)
+        self.val_ssim(ssim_value)
+        self.val_psnr(psnr_value)
+
+        self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/ssim", self.val_ssim, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/psnr", self.val_psnr, on_step=False, on_epoch=True, prog_bar=True)
+        wandb.log({
+            "val/loss": self.val_loss.compute(), 
+            "val/ssim": self.val_ssim.compute(), 
+            "val/psnr": self.val_psnr.compute()
+        })
+
+    def on_validation_epoch_end(self) -> None:
+        "Lightning hook that is called when a validation epoch ends."
+        ssim = self.val_ssim.compute()  # get current val SSIM
+        self.val_ssim_best(ssim)  # update best so far val SSIM
+        # log `val_acc_best` as a value through `.compute()` method, instead of as a metric object
+        # otherwise metric would be reset by lightning after each epoch
+        self.log("val/ssim_best", self.val_ssim_best.compute(), sync_dist=True, prog_bar=True)
+
+
+    def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
+        """Perform a single test step on a batch of data from the test set.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target
+            labels.
+        :param batch_idx: The index of the current batch.
+        """
+        loss, restored, targets = self.model_step(batch)
+
+        # Calculate SSIM and PSNR
+        ssim_value = structural_similarity_index_measure(restored, targets)
+        psnr_value = peak_signal_noise_ratio(restored, targets)
+
+        # update and log metrics
+        self.test_loss(loss)
+        self.test_ssim(ssim_value)
+        self.test_psnr(psnr_value)
+
+        self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/ssim", self.test_ssim, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/psnr", self.test_psnr, on_step=False, on_epoch=True, prog_bar=True)
+        wandb.log({
+            "test/loss": self.test_loss.compute(), 
+            "test/ssim": self.test_ssim.compute(), 
+            "test/psnr": self.test_psnr.compute()
+        })
+
+    def on_test_epoch_end(self) -> None:
+        """Lightning hook that is called when a test epoch ends."""
+        pass
+
+    def setup(self, stage: str) -> None:
+        """Lightning hook that is called at the beginning of fit (train + validate), validate,
+        test, or predict.
+
+        This is a good hook when you need to build models dynamically or adjust something about
+        them. This hook is called on every process when using DDP.
+
+        :param stage: Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
+        """
+        if self.hparams.compile and stage == "fit":
+            self.net = torch.compile(self.net)
+
+    def configure_optimizers(self) -> Dict[str, Any]:
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        Examples:
+            https://lightning.ai/docs/pytorch/latest/common/lightning_module.html#configure-optimizers
+
+        :return: A dict containing the configured optimizers and learning-rate schedulers to be used for training.
+        """
+        optimizer = self.hparams.optimizer(params=self.trainer.model.parameters())
+        if self.hparams.scheduler is not None:
+            scheduler = self.hparams.scheduler(optimizer=optimizer)
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": {
+                    "scheduler": scheduler,
+                    "monitor": "val/loss",
+                    "interval": "epoch",
+                    "frequency": 1,
+                },
+            }
+        return {"optimizer": optimizer}
+
+
+if __name__ == "__main__":
+    _ = UnetModule(None, None, None, None)
